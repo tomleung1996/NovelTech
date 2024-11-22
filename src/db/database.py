5bdb8560dda163e7da6c4c30f8c699e23225cd09
tomleung1996 <tomleung1996@qq.com>
2024-11-22 02:52:35 +0000

    catch error

--- src/db/database.py
+++ src/db/database.py
@@ -4,27 +4,44 @@ from src.utils.logger import logger
 
 class Database:
     def __init__(self, config_path='config/config.yaml'):
-        with open(config_path, mode='r', encoding='utf-8') as f:
-            config = yaml.safe_load(f)
-            db_config = config['database']
+        try:
+            with open(config_path, mode='r', encoding='utf-8') as f:
+                config = yaml.safe_load(f)
+                db_config = config['database']
 
-            self.connection = psycopg.connect(
-                dbname=db_config['db_name'],
-                user=db_config['user'],
-                password=db_config['password'],
-                host=db_config['host'],
-                port=db_config['port']
-            )
+                self.connection = psycopg.connect(
+                    dbname=db_config['db_name'],
+                    user=db_config['user'],
+                    password=db_config['password'],
+                    host=db_config['host'],
+                    port=db_config['port']
+                )
+        except (psycopg.Error, FileNotFoundError, KeyError) as e:
+            logger.error(f'数据库连接失败: {e}')
+            self.connection = None
 
     def fetch(self, query):
+        if not self.connection:
+            logger.error('数据库连接未建立')
+            return None
+
         with self.connection.cursor() as cursor:
             try:
                 cursor.execute(query)
                 return cursor.fetchall()
             except psycopg.Error as e:
                 logger.error(f'执行查询失败: {e}')
-                print(f'执行查询失败: {e}')
                 return None
     
     def close_connection(self):
-        self.connection.close()
+        if self.connection:
+            self.connection.close()
+        else:
+            logger.error('数据库连接未建立或已关闭')
+
+if __name__ == '__main__':
+    db = Database()
+    result = db.fetch('SELECT * FROM cnipa_appl LIMIT 10')
+    db.close_connection()
+    for row in result:
+        print(row)
