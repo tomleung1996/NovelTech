00ccd6bfa326e35e4916935be7c22c839d22d7d4
tomleung1996 <tomleung1996@qq.com>
2024-12-20 12:05:02 +0800

    bug fix

--- src/processing/novel_noun_phrase_filter.py
+++ src/processing/novel_noun_phrase_filter.py
@@ -8,20 +8,12 @@ class NovelNounPhraseFilter:
     def __init__(self, config_path='config/config.yaml'):
         with open(config_path, mode='r', encoding='utf-8') as f:
             config = yaml.safe_load(f)
-            self.existing_noun_phrase_path = config['output_paths']['existing_noun_phrases']
+            self.existing_noun_phrase_set_path = config['output_paths']['existing_noun_phrase_set']
             self.current_noun_phrases_path = config['output_paths']['current_noun_phrases']
-            self.current_novel_noun_phrases_path = config['output_paths']['current_novel_noun_phrases']
+            self.output_path = config['output_paths']['current_novel_noun_phrases']
 
-            self.existing_noun_phrase_set = set()
             logger.info('正在统计现存名词短语……')
-            with open(self.existing_noun_phrase_path, mode='r', encoding='utf-8') as f:
-                for line in tqdm(f):
-                    _, ti_np_list, abs_np_list = line.strip().split('\t')
-                    ti_np_list = ast.literal_eval(ti_np_list)
-                    abs_np_list = ast.literal_eval(abs_np_list)
-
-                    self.existing_noun_phrase_set.update(ti_np_list)
-                    self.existing_noun_phrase_set.update(abs_np_list)
+            self.existing_noun_phrase_set = pickle.load(open(self.existing_noun_phrase_set_path, mode='rb'))
             
             logger.info(f'现存名词短语共 {len(self.existing_noun_phrase_set)} 个')
 
@@ -42,13 +34,15 @@ class NovelNounPhraseFilter:
     def filter_novel_noun_phrases(self):
         logger.info('正在过滤新颖名词短语……')
         for uuid, np_list in tqdm(self.current_noun_phrase_dict.items()):
+            new_np_list = []
             for np in np_list:
-                if np in self.existing_noun_phrase_set:
-                    np_list.remove(np)
+                if np not in self.existing_noun_phrase_set:
+                    new_np_list.append(np)
+            self.current_noun_phrase_dict[uuid] = new_np_list
         logger.info('过滤完成')
 
         logger.info('正在保存新颖名词短语……')
-        with open(self.current_novel_noun_phrases_path, mode='w', encoding='utf-8') as f:
+        with open(self.output_path, mode='w', encoding='utf-8') as f:
             for uuid, np_list in tqdm(self.current_noun_phrase_dict.items()):
                 f.write(f'{uuid}\t{np_list}\n')
         logger.info('保存完成')
